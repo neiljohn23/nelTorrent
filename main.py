5189e0a2aedcfb63a6378bb17d70c8038563eccf
neiljohn23 <neiljohn23@aol.com>
2020-11-27 19:57:01 -0500

    added torrent files

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
+def decode_torrent(filename):
+    with open(filename) as fp:
+        data = fp.read()
+    print(data)
+
 def main():
     print("Welcome to Nel n Friends Torrent Client")
     link = input("please provide the link to your torrent: ")
